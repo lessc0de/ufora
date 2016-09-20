f5801930def77986a0191cc0b424a18c6255542d
Braxton Mckee <braxton.mckee@gmail.com>
2016-09-20 18:07:34 -0400

    [nft] start to implement implval->binary in c++

--- ufora/FORA/python/PurePython/Converter.py
+++ ufora/FORA/python/PurePython/Converter.py
@@ -722,7 +722,7 @@ class Converter(object):
                     stream.defineStacktrace(objId, stackTraceAsJsonOrNone)
                     return objId
                 else:
-                    assert False, "unknown tuple, but not a stacktra"
+                    assert False, "unknown tuple, but not a stacktrace"
 
             if implval.isObject():
                 objectClass = implval.getObjectClass()
